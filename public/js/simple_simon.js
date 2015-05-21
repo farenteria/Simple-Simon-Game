d535fb95022c4b5a56218fb93b6357f35f30196c
Fernando Renteria <rentfern@gmail.com>
2015-05-21 13:37:12 -0500

    Changed JS file name. Also added Jquery UI library

---
+++ public/js/simple_simon.js
@@ -0,0 +1,15 @@
+"use strict";
+(function(){
+	var colors = ["green", "red", "yellow", "blue"];
+	var playing = false;
+	var colorQueue = [];
+
+	function playGame(){
+		var random;
+
+		while(playing){
+			random = Math.floor(Math.random() * colors.length);
+			
+		}
+	}
+})();
