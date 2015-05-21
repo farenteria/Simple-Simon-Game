5ea53fb316f1cc8029380c76ce4e1b29a8350747
Fernando Renteria <rentfern@gmail.com>
2015-05-21 18:43:17 -0500

    Added function for highlighting. Very messy, must refactor to make everything work properly

--- public/js/simple_simon.js
+++ public/js/simple_simon.js
@@ -4,26 +4,64 @@
 	var playing = false;
 	var colorQueue = [];
 	var startButton = $("#start-button");
+	var colorButtonsQueue = [];
+	var index = 0;
+	var highlightColor;
+	// var intervalId;
 
 	function playGame(){
 		var i = 0;
 		var interval = 1000;
+
+		//change to playing
 		while(i < 5){
 			addNewColor();
-			console.log(colorQueue);
 			i++;
 
-			setInterval(function(){
-
+			var intervalId = setInterval(function(){
+				if(index < colorButtonsQueue.length){
+					highlighter();
+					console.log("colorButtonsQueue[index].html + highlighted");
+					index++;
+				} else{
+					clearInterval(intervalId);
+					console.log(colorButtonsQueue.length);
+				}
 			}, interval);
+
+			index = 0;
 		}
 	}
 
+	// a random color will be pushed to our colorButtons array
 	function addNewColor(){
-		var random;
-		random = Math.floor(Math.random() * colors.length);
+		var id;
+		var random = Math.floor(Math.random() * colors.length);
 
-		colorQueue.push(colors[random]);
+		switch(random){
+			case 0:
+				id = $("#green");
+				highlightColor = "#12ea45";
+				break;
+			case 1:
+				id = $("#red");
+				highlightColor = "#f90909";
+				break;
+			case 2:
+				id = $("#yellow");
+				highlightColor = "#ffffd3";
+				break;
+			case 3:
+				id = $("#blue");
+				highlightColor = "#4989ff";
+				break;
+		}
+
+		colorButtonsQueue.push(id);
+	}
+
+	function highlighter(){
+		$(colorButtonsQueue[index]).effect("highlight", highlightColor);
 	}
 
 	startButton.click(function(){
